b0043634a7f2242c59fc87b8c057c93e8e968e5d
Selvakumar Murugesan <selvaonline@gmail.com>
2025-02-24 07:38:18 -0600

    Configure for Heroku deployment

--- server/index.ts
+++ server/index.ts
@@ -1,3 +1,4 @@
+import "dotenv/config";
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
@@ -56,9 +57,8 @@ app.use((req, res, next) => {
     serveStatic(app);
   }
 
-  // ALWAYS serve the app on port 5000
-  // this serves both the API and the client
-  const port = 5000;
+  // Use Heroku's dynamic PORT or fallback to 5050
+  const port = process.env.PORT || 5050;
   server.listen({
     port,
     host: "0.0.0.0",
